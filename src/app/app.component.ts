69a6a063fc212898171255d16760c7873b909765
Bruno Pinheiro <bruno.faria.pinheiro@gmail.com>
2023-06-01 12:01:26 +0100

    added offline capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,68 +27,67 @@ export class AppComponent {
     } else if (!this.isRadioChecked('question3')) {
       alert(alertMessage);
       return;
+    } else if (!this.isRadioChecked('question4')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question5')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question6')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question7')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question8')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question9')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question10')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question11')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question12')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question13')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question14')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question15')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question16')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question17')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question18')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question19')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question20')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question21')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question22')) {
+      alert(alertMessage);
+      return;
+    } else if (!this.isRadioChecked('question23')) {
+      alert(alertMessage);
+      return;
     }
-    // } else if (!this.isRadioChecked('question4')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question5')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question6')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question7')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question8')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question9')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question10')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question11')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question12')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question13')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question14')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question15')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question16')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question17')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question18')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question19')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question20')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question21')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question22')) {
-    //   alert(alertMessage);
-    //   return;
-    // } else if (!this.isRadioChecked('question23')) {
-    //   alert(alertMessage);
-    //   return;
-    // }
 
     this.checkResults();
   }
@@ -99,26 +98,26 @@ export class AppComponent {
     this.checkQuestion(document.getElementsByName('question1'), 1);
     this.checkQuestion(document.getElementsByName('question2'), 2);
     this.checkQuestion(document.getElementsByName('question3'), 3);
-    // this.checkQuestion(document.getElementsByName('question4'), 4);
-    // this.checkQuestion(document.getElementsByName('question5'), 5);
-    // this.checkQuestion(document.getElementsByName('question6'), 6);
-    // this.checkQuestion(document.getElementsByName('question7'), 7);
-    // this.checkQuestion(document.getElementsByName('question8'), 8);
-    // this.checkQuestion(document.getElementsByName('question9'), 9);
-    // this.checkQuestion(document.getElementsByName('question10'), 10);
-    // this.checkQuestion(document.getElementsByName('question11'), 11);
-    // this.checkQuestion(document.getElementsByName('question12'), 12);
-    // this.checkQuestion(document.getElementsByName('question13'), 13);
-    // this.checkQuestion(document.getElementsByName('question14'), 14);
-    // this.checkQuestion(document.getElementsByName('question15'), 15);
-    // this.checkQuestion(document.getElementsByName('question16'), 16);
-    // this.checkQuestion(document.getElementsByName('question17'), 17);
-    // this.checkQuestion(document.getElementsByName('question18'), 18);
-    // this.checkQuestion(document.getElementsByName('question19'), 19);
-    // this.checkQuestion(document.getElementsByName('question20'), 20);
-    // this.checkQuestion(document.getElementsByName('question21'), 21);
-    // this.checkQuestion(document.getElementsByName('question22'), 22);
-    // this.checkQuestion(document.getElementsByName('question23'), 23);
+    this.checkQuestion(document.getElementsByName('question4'), 4);
+    this.checkQuestion(document.getElementsByName('question5'), 5);
+    this.checkQuestion(document.getElementsByName('question6'), 6);
+    this.checkQuestion(document.getElementsByName('question7'), 7);
+    this.checkQuestion(document.getElementsByName('question8'), 8);
+    this.checkQuestion(document.getElementsByName('question9'), 9);
+    this.checkQuestion(document.getElementsByName('question10'), 10);
+    this.checkQuestion(document.getElementsByName('question11'), 11);
+    this.checkQuestion(document.getElementsByName('question12'), 12);
+    this.checkQuestion(document.getElementsByName('question13'), 13);
+    this.checkQuestion(document.getElementsByName('question14'), 14);
+    this.checkQuestion(document.getElementsByName('question15'), 15);
+    this.checkQuestion(document.getElementsByName('question16'), 16);
+    this.checkQuestion(document.getElementsByName('question17'), 17);
+    this.checkQuestion(document.getElementsByName('question18'), 18);
+    this.checkQuestion(document.getElementsByName('question19'), 19);
+    this.checkQuestion(document.getElementsByName('question20'), 20);
+    this.checkQuestion(document.getElementsByName('question21'), 21);
+    this.checkQuestion(document.getElementsByName('question22'), 22);
+    this.checkQuestion(document.getElementsByName('question23'), 23);
 
     this.printTotalFails();
     this.setFocusOnResult();
@@ -130,10 +129,6 @@ export class AppComponent {
 
   printTotalFails() {
     this.showResult = true;
-    // document.getElementById("result").innerHTML = `Total de falhas: ${this.totalFails}`;
-    // document.getElementById("result").style.fontSize = '1.5em';
-    // document.getElementById("result").style.margin = '2em 0';
-    // document.getElementById("result").style.fontWeight = 'bold';
   }
 
   checkQuestion(option: any, failsListPosition: number) {
